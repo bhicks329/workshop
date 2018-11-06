f9cfee57fe0f94417496becae08bcb2f065b2a84
Ben Hicks <bhicks329@gmail.com>
2018-11-06 05:40:00 +0000

    updating registry name

--- tf/src/environment/data.tf
+++ tf/src/environment/data.tf
@@ -27,6 +27,7 @@ data "template_file" "pipeline_credentials" {
     baseregistry-username = "${azurerm_container_registry.acr.admin_username}"
     baseregistry-password = "${azurerm_container_registry.acr.admin_password}"
     baseregistry-url      = "${azurerm_container_registry.acr.login_server}"
+    baseregistry-name     = "${azurerm_container_registry.acr.name}"
     kubeconfig            = "${data.azurerm_kubernetes_cluster.cluster.kube_config_raw}"
   }
 }
